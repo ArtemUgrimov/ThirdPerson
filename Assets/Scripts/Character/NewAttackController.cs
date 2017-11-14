3cc229b60db81509d493db9ab3022f1d66064f92
Artmaster <master.brovary@gmail.com>
2017-11-14 21:58:26 +0200

    progress

--- Assets/Scripts/Character/NewAttackController.cs
+++ Assets/Scripts/Character/NewAttackController.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class NewAttackController : MonoBehaviour {
+public class NewAttackController : NetworkBehaviour {
 
 	static float CROSSFADE_TIME = 0.2f;
 	static int LAYER = 0;
 	static string PREFIX = "Base Layer.Sword.Combat.Attack";
+
+	IEnumerator currentCoroutine = null;
+	IEnumerator attackStopRoutine = null;
+
+	bool attackDone = true;
+	bool attacking = false;
 
 	Animator animator;
 
@@ -16,21 +23,53 @@ public class NewAttackController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
+		if (isLocalPlayer && Input.GetButtonDown ("Fire1") && attackDone) {
+			if (attackStopRoutine != null) {
+				StopCoroutine (attackStopRoutine);
+				attackStopRoutine = null;
+			}
+
+			attacking = true;
 			string anim = GetRandomAttackAnim ();
 			Debug.Log (anim);
-			animator.CrossFade (anim, CROSSFADE_TIME, LAYER);
-			var routine = PlayAnim (CROSSFADE_TIME, anim);
-			StartCoroutine (routine);
+			animator.CrossFade (anim, GetCrossfadeTime(), LAYER);
+			currentCoroutine = PlayAnim (CROSSFADE_TIME, anim);
+			StartCoroutine (currentCoroutine);
 		}
 	}
 
 	IEnumerator PlayAnim(float delta, string name) {
 		yield return new WaitForSeconds (delta);
-		animator.Play (name, LAYER);
+		animator.Play (name, LAYER, GetStartTime());
+		currentCoroutine = null;
+		attackDone = false;
+	}
+
+	IEnumerator AttackStop(float delta) {
+		yield return new WaitForSeconds (delta);
+		attacking = false;
+		attackStopRoutine = null;
+	}
+
+	float GetCrossfadeTime() {
+		return attacking ? CROSSFADE_TIME : CROSSFADE_TIME * 2.5f;
+	}
+
+	float GetStartTime() {
+		return attacking ? 0.2f : 0.0f;
 	}
 
 	string GetRandomAttackAnim() {
-		return PREFIX + Random.Range (1, 5).ToString() + "_0";
+		return PREFIX + Random.Range (1, 6).ToString() + "_0";
+	}
+
+	void SendEvent(string ev) {
+		if (ev == "toStrike") {
+			attackDone = true;
+			var info = animator.GetCurrentAnimatorStateInfo (LAYER);
+			float endTime = info.length * (1 - info.normalizedTime);
+			attackStopRoutine = AttackStop (endTime);
+			StartCoroutine (attackStopRoutine);
+		}
 	}
 }
